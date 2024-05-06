0ec8803287abbff36658a3094a5a33762dc1c460
Juan Morales <juanskate326@gmail.com>
2024-05-06 15:07:15 -0500

    direccion

--- src/components/DashboardComponents/MapaComponent/DirectionsComponent/DirectionComponent.tsx
+++ src/components/DashboardComponents/MapaComponent/DirectionsComponent/DirectionComponent.tsx
@@ -8,7 +8,7 @@ export default function DirectionComponent() {
     const { itemSidebarRight } = useSystemStore()
 
 
-    const [directionsResponse, setDirectionsResponse] = useState<any[] | null>(null)
+    const [directionsResponse, setDirectionsResponse] = useState<any[]>([])
     const [directionTrazado, setDirectionTrazado] = useState<any>(null)
     const [distance, setDistance] = useState<any>('')
     const [duration, setDuration] = useState<any>('')
@@ -64,33 +64,37 @@ export default function DirectionComponent() {
         }
 
         const directionsService = new google.maps.DirectionsService()
+        if (directionsResponse.length === 0) {
 
-        const results: any = await Promise.all(waypointsGroups.map(async (waypointsGroup) => {
-            const results2 = await directionsService.route(
-                {
-                    origin: {
-                        lat: Number.parseFloat(`${waypointsGroup[0].point.PUN_LATITUD}`),
-                        lng: Number.parseFloat(`${waypointsGroup[0].point.PUN_LONGITUD}`)
-                    },
-                    destination: {
-                        lat: Number.parseFloat(`${waypointsGroup[waypointsGroup.length - 1].point.PUN_LATITUD}`),
-                        lng: Number.parseFloat(`${waypointsGroup[waypointsGroup.length - 1].point.PUN_LONGITUD}`)
-                    },
-                    travelMode: google.maps.TravelMode.DRIVING,
-                    waypoints: waypointsGroup.slice(1, waypointsGroup.length - 1).map((waypoint: any) => {
-                        return {
-                            location: {
-                                lat: Number.parseFloat(`${waypoint.point.PUN_LATITUD}`),
-                                lng: Number.parseFloat(`${waypoint.point.PUN_LONGITUD}`)
-                            },
-                            stopover: true
-                        }
-                    }),
-                }
+            await Promise.all(waypointsGroups.map(async (waypointsGroup) => {
+                const results2 = await directionsService.route(
+                    {
+                        origin: {
+                            lat: Number.parseFloat(`${waypointsGroup[0].point.PUN_LATITUD}`),
+                            lng: Number.parseFloat(`${waypointsGroup[0].point.PUN_LONGITUD}`)
+                        },
+                        destination: {
+                            lat: Number.parseFloat(`${waypointsGroup[waypointsGroup.length - 1].point.PUN_LATITUD}`),
+                            lng: Number.parseFloat(`${waypointsGroup[waypointsGroup.length - 1].point.PUN_LONGITUD}`)
+                        },
+                        travelMode: google.maps.TravelMode.DRIVING,
+                        waypoints: waypointsGroup.slice(1, waypointsGroup.length - 1).map((waypoint: any) => {
+                            return {
+                                location: {
+                                    lat: Number.parseFloat(`${waypoint.point.PUN_LATITUD}`),
+                                    lng: Number.parseFloat(`${waypoint.point.PUN_LONGITUD}`)
+                                },
+                                stopover: true,
+
+                            }
+                        }),
+                    }
+                )
+                setDirectionsResponse(directionsResponse => [...directionsResponse, ...results2.routes[0].legs])
+                return results2
+            })
             )
-            return results2
-        })
-        )
+        }
         if (waypointsTrazados.length !== 0) {
             const restulTrazado = await directionsService.route({
                 origin: {
@@ -120,7 +124,8 @@ export default function DirectionComponent() {
 
             setDirectionTrazado(restulTrazado);
         }
-        setDirectionsResponse(results)
+        console.log(directionsResponse);
+
         setOriginRef({
             current: {
                 value: {
@@ -141,7 +146,7 @@ export default function DirectionComponent() {
     }
 
     function clearRoute() {
-        setDirectionsResponse(null)
+        setDirectionsResponse([])
         setDistance('')
         setDuration('')
         setOriginRef({
@@ -173,12 +178,29 @@ export default function DirectionComponent() {
             {
                 (itemSidebarRight && directionsResponse) && (
                     <>
-                        {directionsResponse.map((directions, index) => (
+                        {directionsResponse.map((route, routeIndex) => {
+                            const waypoints = route.steps.slice(0, 1).flatMap(step => step.start_location)
+
+                            return waypoints.slice(0, 1).map((location, waypointIndex) => (
+                                <Marker
+                                    key={`${routeIndex}-${waypointIndex}`}
+                                    position={{ lat: location.lat(), lng: location.lng() }}
+                                    label={{
+                                        text: `Waypoint ${routeIndex + 1}`,  // Aquí puedes personalizar el texto
+                                        color: "#000000",
+                                        fontSize: "18px",
+                                        fontWeight: "bold",
+                                        className: "mb-4"
+                                    }}
+                                />
+                            ));
+                        })}
+                        {/* {directionsResponse.map((data, index) => (
 
                             <DirectionsRenderer
                                 key={index}
                                 options={{
-                                    directions,
+                                    directions: data.directions,
                                     draggable: false,
                                     panel: document.getElementById('panel'),
                                     polylineOptions: {
@@ -187,13 +209,27 @@ export default function DirectionComponent() {
                                         strokeWeight: 6,
                                         zIndex: 1
                                     },
-                                    preserveViewport: true
+                                    preserveViewport: true,
+                                    suppressMarkers: false,
+                                    markerOptions: {
+                                        label: {
+                                            text: `${self}`,
+                                            color: "#000000",
+                                            fontSize: "18px",
+                                            fontWeight: "bold",
+                                            className: "mb-4"
+                                        },
 
+                                        icon: {
+                                            url: "/assets/Recursos/mapa/clusters/location_gen.png",  // URL del icono personalizado para los waypoints
+                                            scaledSize: new window.google.maps.Size(50, 50)  // Tamaño del icono
+                                        }
+                                    }
                                 }}
                             />
 
 
-                        ))}
+                        ))} */}
                         {
                             directionTrazado && (
 
@@ -209,7 +245,13 @@ export default function DirectionComponent() {
                                             zIndex: 2
                                         },
                                         preserveViewport: true,
-                                        suppressMarkers: true
+                                        suppressMarkers: true,
+                                        markerOptions: {
+                                            icon: {
+                                                url: "/assets/Recursos/mapa/clusters/location_gen.png",  // URL del icono personalizado para los waypoints
+                                                scaledSize: new window.google.maps.Size(50, 50)  // Tamaño del icono
+                                            }
+                                        }
                                     }}
                                 />
                             )
