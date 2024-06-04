db97a1d520b8a874bcfc0aa77cbaa0ab22c191a6
Jonathan Bohorquez Villamil <jonathan.bohorquez@rmscol.com>
2024-06-04 09:06:11 -0500

    Exportar todosa

--- src/api/conductor.api.ts
+++ src/api/conductor.api.ts
@@ -4,6 +4,10 @@ export const getDrivers = async (page: number = 1, take: number = 10) => {
     return await fetchApiWeb.get(`driver?page=${page}&take=${take}`);
 }
 
+export const findAllDrivers = async () => {
+    return await fetchApiWeb.get(`driver/findAllDrivers`);
+}
+
 export const downloadExcel = async (data: any) => {
     return await fetchApiWeb.downloadExcel("driver/downloadExcel", { dataExport: data });
 };
