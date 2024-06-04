2a4b06798f0d90465fd0f936f7cfaafa02fd71d0
Juan Morales <juanskate326@gmail.com>
2024-06-04 09:10:13 -0500

    icons

--- src/components/shared/Drawer/DrawerContents/ParametrosContent.tsx
+++ src/components/shared/Drawer/DrawerContents/ParametrosContent.tsx
@@ -5,19 +5,19 @@ export default function ParametrosContent() {
 
     const parametros = [
 
-        { title: "Directorio", href: "", icon: <FaFolder /> },
-        { title: "Responsable Protocolo", href: "", icon: <FaUserTie /> },
-        { title: "Actividad", href: "", icon: <FaTasks /> },
-        { title: "Protocolo", href: "", icon: <FaBook /> },
-        { title: "Categoria Novedad", href: "", icon: <FaTag /> },
-        { title: "Subcategoria Novedad", href: "", icon: <FaTags /> },
-        { title: "Motivo Viaticos", href: "", icon: <FaMoneyBillWave /> },
-        { title: "Categoria Pre-Operacional", href: "", icon: <FaTools /> },
-        { title: "Conductores", href: "/parametros/conductores", icon: <FaCar /> },
-        { title: "Subcategoria Pre-Operacional", href: "", icon: <FaWrench /> },
-        { title: "Puntos", href: "", icon: <FaMapMarkerAlt /> },
-        { title: "Itinerario", href: "", icon: <FaRoute /> },
-        { title: "Vehiculos", href: "", icon: <FaTruck /> },
+        { title: "Directorio", href: "", icon: <FaFolder className="w-6 h-auto" /> },
+        { title: "Responsable Protocolo", href: "", icon: <FaUserTie className="w-6 h-auto" /> },
+        { title: "Actividad", href: "", icon: <FaTasks className="w-6 h-auto" /> },
+        { title: "Protocolo", href: "", icon: <FaBook className="w-6 h-auto" /> },
+        { title: "Categoria Novedad", href: "", icon: <FaTag className="w-6 h-auto" /> },
+        { title: "Subcategoria Novedad", href: "", icon: <FaTags className="w-6 h-auto" /> },
+        { title: "Motivo Viaticos", href: "", icon: <FaMoneyBillWave className="w-6 h-auto" /> },
+        { title: "Categoria Pre-Operacional", href: "", icon: <FaTools className="w-6 h-auto" /> },
+        { title: "Conductores", href: "/parametros/conductores", icon: <FaCar className="w-6 h-auto" /> },
+        { title: "Subcategoria Pre-Operacional", href: "", icon: <FaWrench className="w-6 h-auto" /> },
+        { title: "Puntos", href: "", icon: <FaMapMarkerAlt className="w-6 h-auto" /> },
+        { title: "Itinerario", href: "", icon: <FaRoute className="w-6 h-auto" /> },
+        { title: "Vehiculos", href: "", icon: <FaTruck className="w-6 h-auto" /> },
     ]
 
     return (
